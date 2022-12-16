7b6cb0eef1060c6a6092827fb4c40f5bc6498cbf
Pinteala <120668236+Pinteala@users.noreply.github.com>
2022-12-16 11:41:14 +0200

    Update sudoku game.py

--- sudoku game.py
+++ sudoku game.py
@@ -9,34 +9,6 @@ board =[
     [1, 7, 0, 0, 4, 6, 0, 0, 2],
     [5, 0, 0, 0, 3, 2, 0, 0, 2],
 ]
-def solve(bo):
-    find = find_empty(bo)
-    if not find:
-        return True
-    else:
-        rand, col = find
-    for i in range(1,10):
-        if valid(bo, i, (rand, col)):
-            bo[rand][col] = i
-            if solve(bo):
-                return True
-            bo[rand][col] = 0
-    return False
-
-def valid(bo, num, pos):
-    for i in range(len(bo[0])):
-        if bo[pos[0]][i] == num and pos[1] != i:
-            return False
-    for i in range(len(bo)):
-        if bo[i][pos[1]] == num and pos[0] != i:
-            return False
-    box_x = pos[1] // 3
-    box_y = pos[0] // 3
-    for i in range(box_y*3, box_y*3 + 3):
-        for j in range(box_x * 3, box_x*3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
-                return False
-    return True
 
 def print_board(bo):
     for i in range(len(bo)):
@@ -56,6 +28,35 @@ def find_empty(bo):
             if bo[i][j] == 0:
                 return (i, j)
     return None
+
+def valid(bo, num, pos):
+    for i in range(len(bo[0])):
+        if bo[pos[0]][i] == num and pos[1] != i:
+            return False
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
+            return False
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
+            if bo[i][j] == num and (i,j) != pos:
+                return False
+    return True
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        rand, col = find
+    for i in range(1,10):
+        if valid(bo, i, (rand, col)):
+            bo[rand][col] = i
+            if solve(bo):
+                return True
+            bo[rand][col] = 0
+    return False
 
 print_board(board)
 solve(board)
